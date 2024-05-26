fdb3d2608a5703befbc171c39c707b8540092d4f
Wesley Rogério S Campos <56031533+WesleyRSC@users.noreply.github.com>
2024-05-26 00:57:34 -0300

    test: add a test for fails

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -2,19 +2,21 @@ import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
   let service: UniqueIdService = null;
-  
+
   beforeEach(() => {
     service = new UniqueIdService();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate a unique id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+   should generate a unique id when called with prefix`, () => {
     const id = service.generateUniqueIdWithPrefix(`app`);
 
     expect(id.startsWith('app-')).toBeTrue();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-    //First Approach
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+   should not generate duplicate IDs when called multiple times`, () => {
+    // First Approach
     // const firstId = service.generateUniqueIdWithPrefix(`app`);
     // const secondId = service.generateUniqueIdWithPrefix(`app`);
     // expect(firstId).not.toEqual(secondId);
@@ -28,7 +30,22 @@ describe(UniqueIdService.name, () => {
     expect(ids.size).toBe(50);
   });
 
-  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generated ids when called`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+  should throw error when called with empty param`, () => {
+    const emptyValues = [null, undefined, ''];
+
+    emptyValues.forEach((emptyValue) => {
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow();
+    });
+
+    // These two approachs are valid
+    // expect(() => service.generateUniqueIdWithPrefix(null)).toThrow();
+    // expect(() => service.generateUniqueIdWithPrefix(undefined)).toThrow();
+    // expect(() => service.generateUniqueIdWithPrefix('')).toThrow();
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+   should return the number of generated ids when called`, () => {
     service.generateUniqueIdWithPrefix(`app`);
     service.generateUniqueIdWithPrefix(`app`);
 
