7580cf1d0d77552f0e89a3de3290d3ae8e1ba884
Wesley Rogério S Campos <56031533+WesleyRSC@users.noreply.github.com>
2024-05-30 22:51:55 -0300

    test: fix test bed adding providers and first test

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,16 +1,22 @@
-import { TestBed } from "@angular/core/testing";
-import { LikeWidgetComponent } from "./like-widget.component";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LikeWidgetComponent } from './like-widget.component';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 
-describe(LikeWidgetComponent.name,()=>{
-    let component: LikeWidgetComponent = null;
+describe(LikeWidgetComponent.name, () => {
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
-    beforeEach(async ()=>{
-        await TestBed.configureTestingModule({
-            declarations:[LikeWidgetComponent],
-        }).compileComponents();
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+    }).compileComponents();
 
-    it('',()=>{
+    fixture = TestBed.createComponent(LikeWidgetComponent);
+  });
 
-    })
-})
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+
+    expect(instance).toBeTruthy();
+  });
+});
